5d4803970d138650e5060999a871f56efe0c5382
Aravindindian29 <aravind95.indian@gmail.com>
2025-02-26 00:45:29 +0530

    Delete oddeven.js

--- oddeven.js
+++
@@ -1,8 +0,0 @@
-function isOddOrEven(number){
-    if(number%2==0){
-        console.log(`The given number ${number} is even`);  
-    }
-    else
-    console.log(`The given number ${number} is odd`);
-}
-isOddOrEven(100);
